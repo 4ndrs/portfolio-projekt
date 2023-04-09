3ee4b4a113ba757e4ee3ea2784b9769b65509b3e
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-04-09 16:02:03 +0200

    Add nav tag to header

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,28 +4,30 @@ import Link from "next/link";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => (
-  <header className={styles.header}>
-    <Link href="/" className={styles.homeLink}>
-      <Image
-        src="/favicon.ico"
-        width={32}
-        height={32}
-        alt="Miku waving hi"
-        className={styles.miku}
-      />
-      <span>4ndrs.dev</span>
-    </Link>
-    <menu className={styles.menu}>
-      <li>
-        <Link href="/#skills">Skills</Link>
-      </li>
-      <li>
-        <Link href="/#projects">Projects</Link>
-      </li>
-      <li>
-        <Link href="/#contact">Contact</Link>
-      </li>
-    </menu>
+  <header>
+    <nav className={styles.navbar}>
+      <Link href="/" className={styles.homeLink}>
+        <Image
+          src="/favicon.ico"
+          width={32}
+          height={32}
+          alt="Miku waving hi"
+          className={styles.miku}
+        />
+        <span>4ndrs.dev</span>
+      </Link>
+      <menu className={styles.menu}>
+        <li>
+          <Link href="/#skills">Skills</Link>
+        </li>
+        <li>
+          <Link href="/#projects">Projects</Link>
+        </li>
+        <li>
+          <Link href="/#contact">Contact</Link>
+        </li>
+      </menu>
+    </nav>
   </header>
 );
 
