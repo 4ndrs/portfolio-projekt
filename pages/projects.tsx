e63c25df3e56d62eeb141f372eb6003a571509ab
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-04-09 17:48:13 +0200

    Add projects page

---
+++ pages/projects.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import UnderConstruction from "@/components/UnderConstruction";
+
+const Projects = () => (
+  <>
+    <Head>
+      <title>Projects</title>
+      <meta name="description" content="under construction" />
+    </Head>
+    <UnderConstruction />
+  </>
+);
+
+export default Projects;
