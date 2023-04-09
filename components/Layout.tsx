6c1a2611df2ce0f41e985bdca56094dec1401232
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-04-09 17:53:55 +0200

    Move meta viewport & favicon to Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,11 +4,16 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 import styles from "./Layout.module.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const Layout = ({ children }: { children: React.ReactNode }) => (
   <div className={`${inter.className} ${styles.container}`}>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
     <Navbar />
     <main>{children}</main>
     <Footer />
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,8 +8,6 @@ const Home = () => (
     <Head>
       <title>4ndrs</title>
       <meta name="description" content="Under construction" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
     </Head>
     <Hero />
     <UnderConstruction />
