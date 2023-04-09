3bdaab2326cb46f574cfc88bcd7d132c22845940
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-04-09 14:01:20 +0200

    Add Layout styles

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,14 @@
 import { Inter } from "next/font/google";
+
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+
+import styles from "./Layout.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const Layout = ({ children }: { children: React.ReactNode }) => (
-  <div className={inter.className}>
+  <div className={`${inter.className} ${styles.container}`}>
     <Navbar />
     <main>{children}</main>
     <Footer />
