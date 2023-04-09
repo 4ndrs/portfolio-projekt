f77228898b2ac46d323558b99725fceecaf00d73
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-04-09 13:53:18 +0200

    Add HeartSVG component

---
+++ components/HeartSVG.tsx
@@ -0,0 +1,18 @@
+const HeartSVG = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={16}
+    height={16}
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <title>love</title>
+    <path
+      fill="#46bfcc"
+      d="M12.39 20.87a.696.696 0 0 1-.78 0C9.764 19.637 2 14.15 2 8.973c0-6.68 7.85-7.75 10-3.25 2.15-4.5 10-3.43 10 3.25 0 5.178-7.764 10.664-9.61 11.895z"
+    />
+  </svg>
+);
+
+export default HeartSVG;
