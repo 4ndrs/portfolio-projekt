d5fa486eca664fce1ac3fb1f1490f118425c88e9
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-04-11 14:10:26 +0200

    Add PseudoImageSVG component

---
+++ components/PseudoImageSVG.tsx
@@ -0,0 +1,30 @@
+type SVGProps = React.SVGProps<SVGSVGElement>;
+
+type Props = {
+  first: string;
+  second: string;
+  [id: string]: SVGProps[keyof SVGProps];
+};
+
+const PseudoImageSVG = ({ first, second, ...otherProps }: Props) => (
+  <svg fill="black" stroke="black" viewBox="0 0 295 190" {...otherProps}>
+    <rect width="100%" height="100%" fill="white" stroke="white" />
+    <text
+      dominantBaseline="middle"
+      x="50%"
+      y="50%"
+      textAnchor="middle"
+      fontSize="36px"
+      fontWeight="700"
+    >
+      <tspan fill="#000" stroke="#000">
+        {first}
+      </tspan>
+      <tspan fill="#14afbd" stroke="#14afbd">
+        {second}
+      </tspan>
+    </text>
+  </svg>
+);
+
+export default PseudoImageSVG;
