a8867673d5daf1856bb67d32e1524587aeed0bf8
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-04-11 16:56:49 +0200

    Add optional frontPage property to projects

--- lib/projects.ts
+++ lib/projects.ts
@@ -9,6 +9,7 @@ const Project = z.object({
   description: z.string(),
   pseudoImageFirst: z.string(),
   pseudoImageSecond: z.string(),
+  frontPage: z.number().optional(),
   sourceCode: z.string(),
   liveSite: z.string(),
   videoShowcase: z.string(),
