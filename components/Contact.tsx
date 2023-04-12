47dd70960ae78260f25c20ec080381c2dc228744
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-04-12 18:43:34 +0200

    Add padding sides to Contact component

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -6,30 +6,32 @@ const Contact = () => {
   };
 
   return (
-    <section className={styles.container}>
-      <h1 className={styles.title} id="contact">
-        Contact
-      </h1>
-      <form onSubmit={handleSubmit} className={styles.form}>
-        <div className={styles.names}>
-          <input
-            placeholder="First Name"
-            className={`${styles.field} ${styles.nameField}`}
+    <section className={styles.section}>
+      <div className={styles.container}>
+        <h1 className={styles.title} id="contact">
+          Contact
+        </h1>
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <div className={styles.names}>
+            <input
+              placeholder="First Name"
+              className={`${styles.field} ${styles.nameField}`}
+            />
+            <input
+              placeholder="Last Name"
+              className={`${styles.field} ${styles.nameField}`}
+            />
+          </div>
+          <input placeholder="Your Email" className={`${styles.field}`} />
+          <textarea
+            placeholder="Message"
+            className={`${styles.field} ${styles.messageField}`}
           />
-          <input
-            placeholder="Last Name"
-            className={`${styles.field} ${styles.nameField}`}
-          />
-        </div>
-        <input placeholder="Your Email" className={`${styles.field}`} />
-        <textarea
-          placeholder="Message"
-          className={`${styles.field} ${styles.messageField}`}
-        />
-        <button type="submit" className={`${styles.button}`}>
-          SEND
-        </button>
-      </form>
+          <button type="submit" className={`${styles.button}`}>
+            SEND
+          </button>
+        </form>
+      </div>
     </section>
   );
 };
