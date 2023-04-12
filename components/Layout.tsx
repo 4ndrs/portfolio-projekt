f4a982c6f4f2acfe91dd4fd3468415a7416071a6
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-04-12 15:30:12 +0200

    Add theme-color

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -12,6 +12,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => (
   <div className={`${inter.className} ${styles.container}`}>
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="theme-color" content="#2d858d" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
     <Navbar />
