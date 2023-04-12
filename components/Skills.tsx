7042959b1bf4bbebc71aa3e53d6f5d6dcead145f
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-04-12 18:36:50 +0200

    Add padding sides to Skills component

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -3,20 +3,22 @@ import Link from "next/link";
 import styles from "./Skills.module.css";
 
 const Skills = ({ skills }: { skills: string[] }) => (
-  <section className={styles.container} id="skills">
-    <h1 className={styles.title}>Skills</h1>
-    <ul className={styles.skills}>
-      {skills.map((skill) => (
-        <li key={skill}>
-          <Link
-            href={`/projects?tag=${encodeURI(skill)}`}
-            className={styles.skill}
-          >
-            {skill}
-          </Link>
-        </li>
-      ))}
-    </ul>
+  <section className={styles.section}>
+    <div className={styles.container} id="skill">
+      <h1 className={styles.title}>Skills</h1>
+      <ul className={styles.skills}>
+        {skills.map((skill) => (
+          <li key={skill}>
+            <Link
+              href={`/projects?tag=${encodeURI(skill)}`}
+              className={styles.skill}
+            >
+              {skill}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   </section>
 );
 
