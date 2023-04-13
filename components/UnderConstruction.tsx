e277ca42fc7a68a2987ac13ed3d278b3c0d253ef
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-04-13 16:03:43 +0200

    Add media query & additional styles to UnderConstruction component

--- components/UnderConstruction.tsx
+++ components/UnderConstruction.tsx
@@ -6,7 +6,12 @@ import mikuCrying from "@/assets/miku_crying.jpg";
 const UnderConstruction = () => (
   <section className={styles.container}>
     <h1 className={styles.text}>Under construction</h1>
-    <Image src={mikuCrying} alt="Picture of Miku crying" placeholder="blur" />
+    <Image
+      src={mikuCrying}
+      alt="Picture of Miku crying"
+      placeholder="blur"
+      className={styles.image}
+    />
   </section>
 );
 
