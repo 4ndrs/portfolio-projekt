f3612e0ecd889cb5fc3f34c19a89d135f7ba2295
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-04-13 16:19:40 +0200

    Add HamburgerSVG component

---
+++ components/HamburgerSVG.tsx
@@ -0,0 +1,20 @@
+const HamburgerSVG = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg width="37" height="17" viewBox="0 0 37 17" fill="none" {...props}>
+    <title>Hamburger menu</title>
+    <path d="M1 1H36" stroke="#43A0B1" strokeWidth="2" strokeLinecap="round" />
+    <path
+      d="M1 7.95764H36"
+      stroke="#43A0B1"
+      strokeWidth="2"
+      strokeLinecap="round"
+    />
+    <path
+      d="M1 15.1395H36"
+      stroke="#43A0B1"
+      strokeWidth="2"
+      strokeLinecap="round"
+    />
+  </svg>
+);
+
+export default HamburgerSVG;
