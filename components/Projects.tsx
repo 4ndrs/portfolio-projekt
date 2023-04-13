bece5853b3de679e5f1a00223cc7c919c6ad58b2
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-04-13 14:57:34 +0200

    Add media queries to Projects component

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import ProjectCard from "./ProjectCard";
 
+import useMediaQuery from "@/hooks/useMediaQuery";
 import styles from "./Projects.module.css";
 
 import type { Project } from "@/lib/projects";
@@ -9,6 +10,11 @@ type ProjectToShow = Omit<Project, "frontPage"> & { frontPage: number };
 type Props = { projects: Project[] };
 
 const Projects = ({ projects }: Props) => {
+  const smolScreen = useMediaQuery("(max-width: 660px)");
+  const mediumScreen = useMediaQuery(
+    "(min-width: 661px) and (max-width: 1055px)"
+  );
+
   const projectsToShow = projects
     .filter(isProjectToShow)
     .sort((a, b) => a.frontPage - b.frontPage);
@@ -25,18 +31,29 @@ const Projects = ({ projects }: Props) => {
         <h1 className="title">Projects I have worked on</h1>
 
         <div className={styles.projects}>
-          <ProjectCard featured project={projectOne} className={styles.one} />
           <ProjectCard
-            layout="sdraiato"
+            layout={mediumScreen ? "sdraiato" : "normal"}
+            featured
+            project={projectOne}
+            className={styles.one}
+          />
+          <ProjectCard
+            layout={smolScreen ? "normal" : "sdraiato"}
             project={projectTwo}
             className={styles.two}
           />
           <ProjectCard
             noImage
+            layout={mediumScreen ? "sdraiato" : "normal"}
             project={projectThree}
             className={styles.three}
           />
-          <ProjectCard noImage project={projectFour} className={styles.four} />
+          <ProjectCard
+            noImage
+            layout={mediumScreen ? "sdraiato" : "normal"}
+            project={projectFour}
+            className={styles.four}
+          />
         </div>
 
         <Link href="/projects" className={styles.more}>
