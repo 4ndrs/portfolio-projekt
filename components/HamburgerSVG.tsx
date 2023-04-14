41c689e0f4a01efda4ad40050135adc9217022fe
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-04-14 14:45:59 +0200

    Add class names to each line in HamburgerSVG component

--- components/HamburgerSVG.tsx
+++ components/HamburgerSVG.tsx
@@ -1,14 +1,37 @@
-const HamburgerSVG = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg width="37" height="17" viewBox="0 0 37 17" fill="none" {...props}>
-    <title>Hamburger menu</title>
-    <path d="M1 1H36" stroke="#43A0B1" strokeWidth="2" strokeLinecap="round" />
+type SVGProps = React.SVGProps<SVGSVGElement>;
+
+type Props = {
+  topClassName?: string;
+  midClassName?: string;
+  botClassName?: string;
+  [id: string]: SVGProps[keyof SVGProps];
+};
+
+const HamburgerSVG = ({
+  topClassName = "",
+  midClassName = "",
+  botClassName = "",
+  ...otherProps
+}: Props) => (
+  <svg width="37" height="17" viewBox="0 0 37 17" fill="none" {...otherProps}>
     <path
+      className={topClassName}
+      d="M1 1H36"
+      stroke="#43A0B1"
+      strokeWidth="2"
+      strokeLinecap="round"
+    />
+
+    <path
+      className={midClassName}
       d="M1 7.95764H36"
       stroke="#43A0B1"
       strokeWidth="2"
       strokeLinecap="round"
     />
+
     <path
+      className={botClassName}
       d="M1 15.1395H36"
       stroke="#43A0B1"
       strokeWidth="2"
