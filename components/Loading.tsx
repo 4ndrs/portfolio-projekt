b6c6c6c48b9cbec87656ce9607c439f6074919e4
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-04-15 20:00:07 +0200

    Add Loading component

---
+++ components/Loading.tsx
@@ -0,0 +1,22 @@
+import styles from "./Loading.module.css";
+
+const Loading = () => (
+  <div className={styles.container}>
+    <span className={styles.n}>N</span>
+    <span className={styles.o}>o</span>
+    <span className={styles.w}>w</span>
+    <> </>
+    <span className={styles.l}>L</span>
+    <span className={styles.o2}>o</span>
+    <span className={styles.a}>a</span>
+    <span className={styles.d}>d</span>
+    <span className={styles.i}>i</span>
+    <span className={styles.n2}>n</span>
+    <span className={styles.g}>g</span>
+    <span className={styles.dot1}>.</span>
+    <span className={styles.dot2}>.</span>
+    <span className={styles.dot3}>.</span>
+  </div>
+);
+
+export default Loading;
