f1618e5962246296c2e42886bc9a13bebfd63c39
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-04-18 16:37:50 +0200

    Add OceanWaveSVG modifications

--- components/OceanWaveSVG.tsx
+++ components/OceanWaveSVG.tsx
@@ -1,6 +1,6 @@
 const OceanWaveSVG = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg fill="blue" viewBox="0 0 300 30" {...props}>
-    <path d="M0 0 C0 0 45 30 155 5 C155 5 168 2.8 173 3.8 C178 3 270 30 300 0" />
+  <svg fill="blue" viewBox="0 0 300 20" {...props}>
+    <path d="M0 0 C0 0 45 20 155 5 C155 5 168 2.8 173 3.8 C178 3 270 20 300 0" />
   </svg>
 );
 
