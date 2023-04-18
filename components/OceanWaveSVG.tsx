8a9336c1f79c1b10db16ae1d40f3b496da3728be
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-04-18 14:53:13 +0200

    Add initial OceanWaveSVG component

---
+++ components/OceanWaveSVG.tsx
@@ -0,0 +1,7 @@
+const OceanWaveSVG = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg fill="blue" viewBox="0 0 300 25" {...props}>
+    <path d="M0 0 C0 0 55 20 155 5 C155 5 168 3 173 3.8 C178 3 280 20 300 0" />
+  </svg>
+);
+
+export default OceanWaveSVG;
