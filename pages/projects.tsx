d38f0af6b28638416ee62349b294291aed391c14
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-04-18 16:32:03 +0200

    Add all projects to the projects page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,17 +1,40 @@
 import Head from "next/head";
-
-import UnderConstruction from "@/components/UnderConstruction";
 import ProjectsHero from "@/components/ProjectsHero";
+import ProjectCard from "@/components/ProjectCard";
 
-const Projects = () => (
-  <>
-    <Head>
-      <title>Projects</title>
-      <meta name="description" content="under construction" />
-    </Head>
-    <ProjectsHero />
-    <UnderConstruction />
-  </>
-);
+import { getAllProjects } from "@/lib/projects";
+
+import styles from "@/styles/ProjectsPage.module.css";
+
+export const getStaticProps = async () => {
+  const projects = getAllProjects();
+
+  return {
+    props: {
+      projects,
+    },
+  };
+};
+
+type Props = Awaited<ReturnType<typeof getStaticProps>>["props"];
+
+const Projects = ({ projects }: Props) => {
+  return (
+    <>
+      <Head>
+        <title>Projects</title>
+        <meta name="description" content="under construction" />
+      </Head>
+      <main>
+        <ProjectsHero />
+        <div className={styles.projects}>
+          {projects.map((project) => (
+            <ProjectCard key={project.title} project={project} />
+          ))}
+        </div>
+      </main>
+    </>
+  );
+};
 
 export default Projects;
