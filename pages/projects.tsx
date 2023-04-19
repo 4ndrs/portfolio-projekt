7595786cf671f47f02187b12c6243a28baaa4053
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-04-19 12:46:45 +0200

    Implement search functionality in projects page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,6 +1,9 @@
+import { useRef, useState } from "react";
+
 import Head from "next/head";
 import ProjectsHero from "@/components/ProjectsHero";
 import ProjectCard from "@/components/ProjectCard";
+import SearchBar from "@/components/SearchBar";
 
 import { getAllProjects } from "@/lib/projects";
 
@@ -19,6 +22,27 @@ export const getStaticProps = async () => {
 type Props = Awaited<ReturnType<typeof getStaticProps>>["props"];
 
 const Projects = ({ projects }: Props) => {
+  const [search, setSearch] = useState("");
+
+  const searchInputRef = useRef<HTMLInputElement>(null);
+  const projectsRef = useRef<HTMLDivElement>(null);
+
+  const handleSearchKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === "Enter" && searchInputRef.current) {
+      searchInputRef.current.blur();
+
+      if (projectsRef.current) {
+        projectsRef.current.scrollIntoView();
+      }
+    }
+  };
+
+  const filteredProjects = projects.filter(
+    (project) =>
+      project.title.toLowerCase().includes(search.toLowerCase()) ||
+      project.description.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <>
       <Head>
@@ -27,8 +51,19 @@ const Projects = ({ projects }: Props) => {
       </Head>
       <main className="main">
         <ProjectsHero />
-        <div className={styles.projects}>
-          {projects.map((project) => (
+
+        <div className={styles.filterContainer}>
+          <SearchBar
+            ref={searchInputRef}
+            placeholder="Search for projects..."
+            value={search}
+            onChange={(event) => setSearch(event.target.value)}
+            onKeyDown={handleSearchKeyDown}
+          />
+        </div>
+
+        <div ref={projectsRef} className={styles.projects}>
+          {filteredProjects.map((project) => (
             <ProjectCard key={project.title} project={project} />
           ))}
         </div>
