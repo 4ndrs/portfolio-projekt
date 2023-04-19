b2e41796cac028241a931daba8c2d81160727427
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-04-19 17:30:49 +0200

    Add MikuSelect component

---
+++ components/MikuSelect.tsx
@@ -0,0 +1,58 @@
+import styles from "./MikuSelect.module.css";
+
+type Props = {
+  className: string;
+  tags: string[];
+  checkedTags: string[];
+  onChange: (checkedTags: string[]) => void;
+};
+
+const MikuSelect = ({ className, tags, checkedTags, onChange }: Props) => {
+  const handleChange = (tag: string) => {
+    if (checkedTags.includes(tag)) {
+      return onChange(checkedTags.filter((checkedTag) => checkedTag !== tag));
+    }
+
+    return onChange([...checkedTags, tag]);
+  };
+
+  return (
+    <>
+      <div className={`${className} ${styles.container}`} tabIndex={0}>
+        <span className={styles.checkedTagsContainer}>
+          Tags:{" "}
+          {checkedTags.length === 0 ? (
+            "Any"
+          ) : (
+            <ul>
+              {checkedTags.map((tag) => (
+                <li key={tag}>{tag}</li>
+              ))}
+            </ul>
+          )}
+        </span>
+        <div className={styles.triangle} />
+        <ul className={styles.tagsContainer}>
+          {tags.map((tag) => (
+            <li key={tag}>
+              <label
+                className={`${styles.tag} ${
+                  checkedTags.includes(tag) ? styles.checked : ""
+                }`}
+              >
+                {tag}
+                <input
+                  checked={checkedTags.includes(tag)}
+                  type="checkbox"
+                  onChange={() => handleChange(tag)}
+                />
+              </label>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default MikuSelect;
