dc8b7048a1c2140dd5e53bf11cae8b99c29c3837
Andres Eloy Rivera Garcia <me@4ndrs.dev>
2023-04-19 17:34:00 +0200

    Implement tag filtering on projects page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -3,26 +3,31 @@ import { useRef, useState } from "react";
 import Head from "next/head";
 import ProjectsHero from "@/components/ProjectsHero";
 import ProjectCard from "@/components/ProjectCard";
+import MikuSelect from "@/components/MikuSelect";
 import SearchBar from "@/components/SearchBar";
 
 import { getAllProjects } from "@/lib/projects";
+import { getAllTags } from "@/utils";
 
 import styles from "@/styles/ProjectsPage.module.css";
 
 export const getStaticProps = async () => {
   const projects = getAllProjects();
+  const tags = getAllTags(projects);
 
   return {
     props: {
       projects,
+      tags,
     },
   };
 };
 
 type Props = Awaited<ReturnType<typeof getStaticProps>>["props"];
 
-const Projects = ({ projects }: Props) => {
+const Projects = ({ projects, tags }: Props) => {
   const [search, setSearch] = useState("");
+  const [checkedTags, setCheckedTags] = useState<string[]>(["TypeScript"]);
 
   const searchInputRef = useRef<HTMLInputElement>(null);
   const projectsRef = useRef<HTMLDivElement>(null);
@@ -39,8 +44,9 @@ const Projects = ({ projects }: Props) => {
 
   const filteredProjects = projects.filter(
     (project) =>
-      project.title.toLowerCase().includes(search.toLowerCase()) ||
-      project.description.toLowerCase().includes(search.toLowerCase())
+      (project.title.toLowerCase().includes(search.toLowerCase()) ||
+        project.description.toLowerCase().includes(search.toLowerCase())) &&
+      checkedTags.every((tag) => project.tags.includes(tag))
   );
 
   return (
@@ -60,6 +66,12 @@ const Projects = ({ projects }: Props) => {
             value={search}
             onChange={(event) => setSearch(event.target.value)}
             onKeyDown={handleSearchKeyDown}
+          />
+          <MikuSelect
+            tags={tags}
+            checkedTags={checkedTags}
+            className={styles.mikuSelect}
+            onChange={(newCheckedTags) => setCheckedTags(newCheckedTags)}
           />
         </div>
 
